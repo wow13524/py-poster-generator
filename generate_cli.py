9cde89537b0595e80a244812f164b0bd2fe18660
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-05-11 23:52:52 -0700

    Formatting

--- generate_cli.py
+++ generate_cli.py
@@ -1,5 +1,5 @@
 import sys
-from argparse import ArgumentParser,Namespace,REMAINDER
+from argparse import ArgumentParser, Namespace, REMAINDER
 from poster_generator.poster_builder import PosterBuilder
 from poster_generator.poster_template import PosterTemplate, PosterTemplateModel
 from poster_generator.type_utils import parse_file
@@ -22,7 +22,7 @@ def _parse_args(args: List[str]) -> Namespace:
 
 if __name__ == "__main__":
     args: Namespace = _parse_args(sys.argv[1:])
-    model: PosterTemplateModel = parse_file(args.template,PosterTemplateModel)
+    model: PosterTemplateModel = parse_file(args.template, PosterTemplateModel)
     template: PosterTemplate = PosterTemplate(model)
-    builder: PosterBuilder = PosterBuilder(template,args.template_args)
+    builder: PosterBuilder = PosterBuilder(template, args.template_args)
     builder.build().write_to_file("TEST.png")
