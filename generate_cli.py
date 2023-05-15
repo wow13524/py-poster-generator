8f90439c43a2da8d4668fc9973e3e7b78eac0548
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-05-14 23:27:24 -0700

    Add debug flag

--- generate_cli.py
+++ generate_cli.py
@@ -10,6 +10,12 @@ def _parse_args(args: List[str]) -> Namespace:
         description="Programmatically generate posters using templates and various online services."
     )
     parser.add_argument(
+        "-d", "--debug",
+        action="store_true",
+        default=False,
+        help="enable debug mode during poster generation"
+    )
+    parser.add_argument(
         "template",
         help="path of the desired poster template"
     )
