1dcfcc928fe866c8f223ed8e71a6f7804d7d8f5e
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-07-28 00:12:18 -0700

    Parse elements and expressions
    I'm thinking of redefining elements as expressions which evaluate to Image.

--- poster_generator/plugin_context.py
+++ poster_generator/plugin_context.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 from inspect import get_annotations, getmembers, isclass
-from typing import Any, Dict, List, Optional, Type, TypeVar, Union
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union, cast
 from .api.models import Element, Expression, Plugin
 from .models import RawObject
 
@@ -31,6 +31,9 @@ class ActiveContext:
         if plugin_class not in self._context:
             raise Exception(f"Could not update {__class__.__name__}: {plugin_class.__name__} is not included in this context")
         self._context[plugin_class] = context
+    
+    def evaluate(self, obj: Union[Element, Expression]) -> Any:
+        pass
 
 class PluginContext:
     def __init__(self, required_plugins: List[str]) -> None:
@@ -54,22 +57,34 @@ class PluginContext:
                         raise Exception(f"Failed to load plugin {plugin_name}.{plugin_name}: {e}")
                     
                     for element_class in plugin_class.elements:
-                        self._element_name_map[element_class.__name__] = element_class
+                        self._element_name_map[f"{plugin_class.__name__}.{element_class.__name__}"] = element_class
                         self._element_plugin_map[element_class] = plugin_class
                     
                     for expression_class in plugin_class.expressions:
-                        self._expression_name_map[expression_class.__name__] = expression_class
+                        self._expression_name_map[f"{plugin_class.__name__}.{expression_class.__name__}"] = expression_class
                         self._expression_plugin_map[expression_class] = plugin_class
     
     def new_active_context(self) -> ActiveContext:
         return ActiveContext(self._element_plugin_map, self._expression_plugin_map, self._plugin_map)
     
+    def _is_raw_object(self, raw_value: Any) -> bool:
+        if type(raw_value) != dict:
+            return False
+        value = cast(Dict[str, Any], raw_value)
+        if type(value.get("type")) != str:
+            return False
+        if type(value.get("args")) != dict:
+            return False
+        if any(type(k) == str for k in cast(Dict[Any, Any], value["args"])):
+            return False
+        return True
+
     def _parse_raw_object(self, raw_obj: RawObject, obj_type: Type[T]) -> T:
-        obj_class: Optional[Type[Union[Element, Expression]]] = None
+        obj_class: Optional[Type[T]] = None
         if obj_type == Element:
-            obj_class = self._element_name_map.get(raw_obj.type)
+            obj_class = cast(Type[T], self._element_name_map.get(raw_obj.type))
         elif obj_type == Expression:
-            obj_class = self._expression_name_map.get(raw_obj.type)
+            obj_class = cast(Type[T], self._expression_name_map.get(raw_obj.type))
         if obj_class is None:
             raise Exception(f"Failed to parse {obj_type.__name__} '{raw_obj.type}': does not exist")
 
@@ -78,5 +93,15 @@ class PluginContext:
             if field in annotations:
                 del annotations[field]
         fields = {
-
+            field: (self._parse_raw_object(value, obj_type) if self._is_raw_object(value) else value) for field,value in raw_obj.args.items()
         }
+
+        obj = obj_class()
+        setattr(obj, "_fields", fields)
+        return obj
+    
+    def parse_element(self, raw_obj: RawObject) -> Element:
+        return self._parse_raw_object(raw_obj, Element)
+    
+    def parse_expression(self, raw_obj: RawObject) -> Expression:
+        return self._parse_raw_object(raw_obj, Expression)
--- poster_generator/poster_generator.py
+++ poster_generator/poster_generator.py
@@ -3,6 +3,7 @@ from dataclasses import asdict
 from PIL import Image
 from pydoc import locate
 from typing import Any, Dict, List, Optional, Type
+from .api.models import Element, Expression
 from .core_plugins import Args
 from .models import PosterTemplate
 from .plugin_context import ActiveContext, PluginContext
@@ -10,6 +11,8 @@ from .plugin_context import ActiveContext, PluginContext
 def generate_poster(template: PosterTemplate, args: List[Any], debug: bool=False) -> Image.Image:
     parser: ArgumentParser = ArgumentParser(template.meta.name)
     plugin_context: PluginContext = PluginContext(template.meta.required_plugins)
+    content: List[Element] = [plugin_context.parse_element(raw_obj) for raw_obj in template.content]
+    logic: List[Expression] = [plugin_context.parse_expression(raw_obj) for raw_obj in template.logic]
     active_context: ActiveContext = plugin_context.new_active_context()
 
     #Build parser
