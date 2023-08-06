b4b15806502ca7ca5b40fa6dffc646eba97747dc
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-08-06 01:13:43 -0700

    Add tuple plugin

--- poster_generator/core_plugins/__init__.py
+++ poster_generator/core_plugins/__init__.py
@@ -1,5 +1,6 @@
 #pyright: reportUnusedImport=false
 from .args import Args
 from .str import Str
+from .tuple import Tuple
 from .ui import Ui
 from .vars import Vars
---
+++ poster_generator/core_plugins/tuple.py
@@ -0,0 +1,24 @@
+from poster_generator.api import Expression, Plugin, REQUIRED, expression
+from sys import maxsize
+from typing import Any, Iterable
+
+class Construct(Expression[tuple[Any, ...], Any]):
+    def evaluate(self, *, context: Any, args: Iterable[Any]=REQUIRED) -> tuple[Any, ...]:
+        return tuple(args)
+
+class Count(Expression[int, None]):
+    def evaluate(self, *, context: None, t: tuple[Any, ...]=REQUIRED, value: Any=REQUIRED) -> int:
+        return t.count(value)
+
+class Index(Expression[int, None]):
+    def evaluate(self, *, context: None, t: tuple[Any, ...]=REQUIRED, value: Any=REQUIRED, start: int=0, stop: int=maxsize) -> int:
+        return t.index(value, start, stop)
+
+@expression(
+    Construct,
+    Count,
+    Index
+)
+class Tuple(Plugin[None]):
+    def new_context(self) -> None:
+        return None
