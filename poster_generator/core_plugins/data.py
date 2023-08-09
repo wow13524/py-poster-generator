4f6071a34ec2b7c462cafaf51ac9e0f4ef9b9b15
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-08-09 00:31:05 -0700

    Remove unused Dimension (Expression) methods

--- poster_generator/core_plugins/data.py
+++ poster_generator/core_plugins/data.py
@@ -5,16 +5,8 @@ from .classes import Dimension as CDimension
 DataContext = NoneType
 
 class Dimension(Expression[CDimension, DataContext]):
-    def __init__(self, px: int=0, percent: float=0) -> None:
-        self._fields = {}
-        self._px = px
-        self._percent = percent
-
     def evaluate(self, *, context: DataContext, px: int=0, percent: float=0) -> CDimension:
         return CDimension(px=px, percent=percent)
-    
-    def to_pixels(self, size: int) -> int:
-        return int(self._px + self._percent * size / 100.)
 
 @expression(
     Dimension
