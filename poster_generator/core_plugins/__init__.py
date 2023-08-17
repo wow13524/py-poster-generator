ff691f762493a89c228e3462edcc4a0f87254474
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-08-16 22:14:03 -0700

    Add math expressions

--- poster_generator/core_plugins/__init__.py
+++ poster_generator/core_plugins/__init__.py
@@ -3,6 +3,7 @@ from .args import Args
 from .data import Data
 from .dict import Dict
 from .list import List
+from .math import Math
 from .set import Set
 from .str import Str
 from .tuple import Tuple
---
+++ poster_generator/core_plugins/math.py
@@ -0,0 +1,37 @@
+from poster_generator.api import Expression, Plugin, REQUIRED, expression
+
+class Add(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a + b
+
+class Sub(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a - b
+
+class Mul(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a * b
+
+class Div(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a / b
+
+class Mod(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a % b
+
+class Pow(Expression[float, None]):
+    def evaluate(self, *, context: None, a: float=REQUIRED, b: float=REQUIRED) -> float:
+        return a ** b
+
+@expression(
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow
+)
+class Math(Plugin[None]):
+    def new_context(self) -> None:
+        return None
