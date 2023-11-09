61cc8424dbefb762059ef7a47eb8a03555bcc8d4
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-11-08 23:57:49 -0800

    Update op.py

--- poster_generator/core_plugins/op.py
+++ poster_generator/core_plugins/op.py
@@ -142,8 +142,8 @@ class Zip(Expression[Iterator[tuple[TAny, TAny]], None]):
         return zip(*args)
 
 class Print(Expression[None, None]):
-    def evaluate(self, *, context: None, values: List[TAny]=[], sep: Optional[str]=" ", end: Optional[str]="\n") -> None:
-        return print(*values, sep=sep, end=end)
+    def evaluate(self, *, context: None, values: List[TAny]=[], sep: Optional[str]=" ", end: Optional[str]="\n", flush: bool=False) -> None:
+        return print(*values, sep=sep, end=end, flush=flush)
 
 class Reversed(Expression[None, None]):
     def evaluate(self, *, context: None, sequence: Reversible[TAny]=REQUIRED) -> None:
